eaf0cebe4673e5880a3c3904dce0b6ff7eea1f1f
DanilaNova <4321danila@gmail.com>
2018-09-30 23:07:38 +0300

    Moved 0.3.7 to own branch

--- 0.4.0/config.js
+++
@@ -1,4 +0,0 @@
-var config = {}
-config.admin = "Venin"
-config.token = null
-module.exports = config
--- 0.4.1/config.js
+++
@@ -1,4 +0,0 @@
-var config = {}
-config.admin = "Venin"
-config.token = "NDkzNzcyMDk1MzIzMzczNTY4.DpKAQQ.ASQvlQGvRDU3yrmhUmMXZIhexrQ"
-module.exports = config
